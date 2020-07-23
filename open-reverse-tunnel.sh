b93640b447325c2f4e46adae72b76a5bf0a8b4ff
Mario HD <mariohdpz@gmail.com>
2020-07-23 12:05:04 -0500

    Check for internet connection

--- open-reverse-tunnel.sh
+++ open-reverse-tunnel.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-set -e
 
 # Use root user
 [ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+
+# Check for internet connection
+echo -e "GET http://google.com HTTP/1.0\n\n" | nc google.com 80 -w 3 > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo >&2 "Must have internet connection!"; exit 1;
+fi
 
 # Install autossh if not installed
 if [ $(dpkg-query -W -f='${Status}' autossh 2>/dev/null | grep -c "ok installed") -eq 0 ];
